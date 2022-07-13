f66cf1bcf836ccd30e7014346b1a1bd5ed2e40da
wangxiaocuo <wangxiaocuo@users.noreply.github.com>
2022-07-13 14:48:08 +0000

    deploy: 2ff4d9a62007b714809921e36adb1097f36afb61

---
+++ assets/blog_20220328-nvm.md.a23fe8fe.js
@@ -0,0 +1,23 @@
+import{_ as s,c as n,o as a,a as l}from"./app.228f4434.js";const _=JSON.parse('{"title":"nvm \u5E38\u7528\u547D\u4EE4","description":"","frontmatter":{"title":"nvm \u5E38\u7528\u547D\u4EE4","tags":["Node.js"]},"headers":[],"relativePath":"blog/20220328-nvm.md"}'),p={name:"blog/20220328-nvm.md"},o=l(`<h1 id="nvm-\u5E38\u7528\u547D\u4EE4" tabindex="-1">nvm \u5E38\u7528\u547D\u4EE4 <a class="header-anchor" href="#nvm-\u5E38\u7528\u547D\u4EE4" aria-hidden="true">#</a></h1><div class="language-shell"><span class="copy"></span><pre><code><span class="line"><span style="color:#7B7F8B;"># \u5B89\u88C5\u6700\u65B0\u7A33\u5B9A\u7248 node</span></span>
+<span class="line"><span style="color:#F6F6F4;">nvm install stable</span></span>
+<span class="line"><span style="color:#7B7F8B;"># \u5B89\u88C5\u6307\u5B9A\u7248\u672C</span></span>
+<span class="line"><span style="color:#F6F6F4;">nvm install </span><span style="color:#F286C4;">&lt;</span><span style="color:#F6F6F4;">version</span><span style="color:#F286C4;">&gt;</span></span>
+<span class="line"><span style="color:#7B7F8B;"># \u5220\u9664\u5DF2\u5B89\u88C5\u7684\u6307\u5B9A\u7248\u672C</span></span>
+<span class="line"><span style="color:#F6F6F4;">nvm uninstall </span><span style="color:#F286C4;">&lt;</span><span style="color:#F6F6F4;">version</span><span style="color:#F286C4;">&gt;</span></span>
+<span class="line"><span style="color:#7B7F8B;"># \u5207\u6362\u4F7F\u7528\u6307\u5B9A\u7684\u7248\u672Cnode</span></span>
+<span class="line"><span style="color:#F6F6F4;">nvm use </span><span style="color:#F286C4;">&lt;</span><span style="color:#F6F6F4;">version</span><span style="color:#F286C4;">&gt;</span></span>
+<span class="line"><span style="color:#7B7F8B;"># \u5217\u51FA\u6240\u6709\u5B89\u88C5\u7684\u7248\u672C</span></span>
+<span class="line"><span style="color:#F6F6F4;">nvm ls</span></span>
+<span class="line"><span style="color:#7B7F8B;"># \u5217\u51FA\u6240\u6709\u8FDC\u7A0B\u670D\u52A1\u5668\u7684\u7248\u672C</span></span>
+<span class="line"><span style="color:#F6F6F4;">nvm ls-remote</span></span>
+<span class="line"><span style="color:#7B7F8B;"># \u663E\u793A\u5F53\u524D\u7684\u7248\u672C</span></span>
+<span class="line"><span style="color:#F6F6F4;">nvm current</span></span>
+<span class="line"><span style="color:#7B7F8B;"># \u7ED9\u4E0D\u540C\u7684\u7248\u672C\u53F7\u6DFB\u52A0\u522B\u540D</span></span>
+<span class="line"><span style="color:#F6F6F4;">nvm </span><span style="color:#97E1F1;">alias</span><span style="color:#F6F6F4;"> </span><span style="color:#F286C4;">&lt;</span><span style="color:#F6F6F4;">name</span><span style="color:#F286C4;">&gt;</span><span style="color:#F6F6F4;"> </span><span style="color:#F286C4;">&lt;</span><span style="color:#F6F6F4;">version</span><span style="color:#F286C4;">&gt;</span></span>
+<span class="line"><span style="color:#7B7F8B;"># \u5220\u9664\u5DF2\u5B9A\u4E49\u7684\u522B\u540D</span></span>
+<span class="line"><span style="color:#F6F6F4;">nvm </span><span style="color:#97E1F1;">unalias</span><span style="color:#F6F6F4;"> </span><span style="color:#F286C4;">&lt;</span><span style="color:#F6F6F4;">name</span><span style="color:#F286C4;">&gt;</span></span>
+<span class="line"><span style="color:#7B7F8B;"># \u5728\u5F53\u524D\u7248\u672C node \u73AF\u5883\u4E0B\uFF0C\u91CD\u65B0   \u5168\u5C40\u5B89\u88C5\u6307\u5B9A\u7248\u672C\u53F7\u7684 npm \u5305</span></span>
+<span class="line"><span style="color:#F6F6F4;">nvm reinstall-packages </span><span style="color:#F286C4;">&lt;</span><span style="color:#F6F6F4;">version</span><span style="color:#F286C4;">&gt;</span></span>
+<span class="line"><span style="color:#7B7F8B;">#\u8BBE\u7F6E\u9ED8\u8BA4\u7248\u672C</span></span>
+<span class="line"><span style="color:#F6F6F4;">nvm </span><span style="color:#97E1F1;">alias</span><span style="color:#F6F6F4;"> default [node\u7248\u672C\u53F7]</span></span>
+<span class="line"></span></code></pre></div>`,2),e=[o];function t(c,F,r,i,y,m){return a(),n("div",null,e)}var d=s(p,[["render",t]]);export{_ as __pageData,d as default};
